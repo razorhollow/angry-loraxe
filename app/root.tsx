0b1b364743224fc6097a263d76360178bc139dbf
RazorHollow <rob@razorhollow.com>
2024-09-16 13:34:46 -0400

    add favicon files

--- app/root.tsx
+++ app/root.tsx
@@ -16,6 +16,15 @@ import stylesheet from "~/tailwind.css";
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+
+  // Favicon links
+  { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+  { rel: "icon", type: "image/png", sizes: "192x192", href: "/android-chrome-192x192.png" },
+  { rel: "icon", type: "image/png", sizes: "512x512", href: "/android-chrome-512x512.png" },
+  { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+  { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
+  { rel: "manifest", href: "/site.webmanifest" },
+  { rel: "shortcut icon", href: "/favicon.ico" },
 ];
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
---
+++ app/routes/_marketing+/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div>
+      <h1>This is the Homepage, Foo</h1>
+    </div>
+  );
+}
