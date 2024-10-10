8715846515a17e4ac394b7b117372bbcda67e7d4
RazorHollow <rob@razorhollow.com>
2024-10-10 09:57:12 -0400

    update bio section

--- app/routes/_landing.about.tsx
+++ app/routes/_landing.about.tsx
@@ -35,17 +35,22 @@ const people = [
         name: 'Mark Sempler',
         role: 'Owner / President',
         imageUrl: markHeadshotImg,
-        bio: 'Praesentium iure error aliquam voluptas ut libero. Commodi placeat sit iure nulla officiis. Ut ex sit repellat tempora. Qui est accusamus exercitationem natus ut voluptas. Officiis velit eos ducimus.',
-        xUrl: '#',
+        bio: 'Mark, one half of the brother duo leading Angry Loraxe, brings a wealth of management experience to the company. Having managed several large corporations, he excels in fostering exceptional customer service and building strong, cohesive teams. Before his foray into management, Mark spent seven years in the oilfield industry. There, he gained invaluable expertise in personal safety protocols, working under high-pressure conditions, and ensuring equipment operated at peak performance through rigorous lab testing and assessments. His diverse background contributes significantly to the high standards and quality of service that Angry Loraxe is known for today.',
         linkedinUrl: 'https://www.linkedin.com/in/mark-sempler-3341b368/',
     },
     {
         name: 'Josh Sempler',
         role: 'Owner / CEO',
         imageUrl: joshHeadShotImg,
-        bio: 'Praesentium iure error aliquam voluptas ut libero. Commodi placeat sit iure nulla officiis. Ut ex sit repellat tempora. Qui est accusamus exercitationem natus ut voluptas. Officiis velit eos ducimus.',
-        xUrl: '#',
-        linkedinUrl: '#',
+        bio: <>Josh, the other brother behind Angry Loraxe, is a dedicated forester with a degree from {" "}
+            <a 
+                href="https://www.paulsmiths.edu/" 
+                className="text-primary-300 hover:text-primary-500" 
+                target="_blank" 
+                rel="noopener noreferrer"
+            >
+                Paul Smith&apos;s College</a>. His education deepened his appreciation for trees and the unique impact each species has on our environment. Following graduation, Josh applied his knowledge at Cummins Lumber, where he honed his skills over several years. Passionate about continual learning, he has earned multiple certifications in the safe and precise felling of trees. Whether working in the woods or engaging in further education, Josh&apos;s commitment to professionalism and environmental stewardship ensures that Angry Loraxe delivers expert tree services with a conscientious approach.</>,
+        linkedinUrl: null,
     },
 ];
 
@@ -89,26 +94,18 @@ export default function AboutPage() {
                                 <p className="text-base leading-7 text-gray-600">{person.role}</p>
                                 <p className="mt-4 text-base leading-7 text-gray-600">{person.bio}</p>
                                 <ul className="mt-6 flex gap-x-6">
-                                    <li>
-                                        <a href={person.xUrl} className="text-gray-400 hover:text-gray-500">
-                                            <span className="sr-only">X</span>
-                                            <svg fill="currentColor" viewBox="0 0 20 20" aria-hidden="true" className="h-5 w-5">
-                                                <path d="M11.4678 8.77491L17.2961 2H15.915L10.8543 7.88256L6.81232 2H2.15039L8.26263 10.8955L2.15039 18H3.53159L8.87581 11.7878L13.1444 18H17.8063L11.4675 8.77491H11.4678ZM9.57608 10.9738L8.95678 10.0881L4.02925 3.03974H6.15068L10.1273 8.72795L10.7466 9.61374L15.9156 17.0075H13.7942L9.57608 10.9742V10.9738Z" />
-                                            </svg>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href={person.linkedinUrl} className="text-gray-400 hover:text-gray-500">
-                                            <span className="sr-only">LinkedIn</span>
-                                            <svg fill="currentColor" viewBox="0 0 20 20" aria-hidden="true" className="h-5 w-5">
-                                                <path
-                                                    d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
-                                                    clipRule="evenodd"
-                                                    fillRule="evenodd"
-                                                />
-                                            </svg>
-                                        </a>
-                                    </li>
+                                    {person.linkedinUrl ? <li>
+                                            <a href={person.linkedinUrl} className="text-gray-400 hover:text-gray-500">
+                                                <span className="sr-only">LinkedIn</span>
+                                                <svg fill="currentColor" viewBox="0 0 20 20" aria-hidden="true" className="h-5 w-5">
+                                                    <path
+                                                        d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
+                                                        clipRule="evenodd"
+                                                        fillRule="evenodd"
+                                                    />
+                                                </svg>
+                                            </a>
+                                        </li> : null}
                                 </ul>
                             </li>
                         ))}
